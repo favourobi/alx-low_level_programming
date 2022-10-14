313d1e9be1275d877d0e4b8c3aff1444f3ea2fad
favourobi <chikkyfavour1@gmail.com>
2022-10-14 16:16:18 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n>0)
   {
-printf("is positive\n", n);
+printf("%i is positive\n", n);
   }
 if (n==0)
    {
-printf("is zero\n", n);
+printf("%i is zero\n", n);
    }
 if (n<0)
    {
-printf("is negative\n", n);
+printf("%i is negative\n", n);
    }
 return (0);
   
