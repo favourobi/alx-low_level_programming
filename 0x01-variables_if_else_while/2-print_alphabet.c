c7f01282df4c54f9cc2b6e4ef1a9f2d7258d4518
favourobi <chikkyfavour1@gmail.com>
2022-10-15 09:11:19 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Start
+ * Return: 0
+ */
+
+int main(void)
+{
+char a_bet = 'a';
+while (a_bet <= 'z')
+{
+putchar(a_bet);
+a_bet++;
+}
+putchar('\n');
+return (0);
+}
