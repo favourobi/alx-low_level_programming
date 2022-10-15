8a54eeefa1a71b3422b2d95ef0c46554747445e8
favourobi <chikkyfavour1@gmail.com>
2022-10-15 10:55:52 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -Start
+ * Return: 0
+ */
+
+int main(void)
+{
+char alpha = 'z';
+while (alpha >= 'a')
+{
+putchar(alpha);
+alpha--;
+}
+putchar('\n');
+return (0);
+}
