e5cad3f2e0ed32fbd789c9044d593d64a185ebee
favourobi <chikkyfavour1@gmail.com>
2022-10-18 16:26:26 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - Start
+ * @c: ASCII Character
+ * Return: 1
+ */
+
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
