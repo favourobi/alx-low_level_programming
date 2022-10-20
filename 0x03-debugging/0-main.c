69fa20995759ee0af838d5d2ab50346e995a5106
favourobi <chikkyfavour1@gmail.com>
2022-10-20 11:34:19 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * main - testing the positive or negative func
- * Return: All good
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
 {
 int i;
+
 i = 0;
 positive_or_negative(i);
-printf("\n");
+
 return (0);
 }
